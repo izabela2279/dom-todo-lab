2951dec23d67198f889cdb2927fb11fbd2f07caa
Izabela <izabelaszeremeta@gmail.com>
2022-12-07 21:08:04 -0500

    Complete steps 5-6

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const linkInput = document.getElementById("list")
+const linkButton = document.getElementById("submit-button")
+const linkUl = document.getElementById("todo-list")
+
+linkButton.addEventListener("click", function(evt){
+  const li = document.createElement("li")
+  li.textContent = linkInput.value
+  linkInput.text = ""
+  linkUl.appendChild(li)
+})
